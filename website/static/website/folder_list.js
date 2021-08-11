e56db316dc2f8eb7b3fa2be7c1229569214c4f5d
James Herring <jamesherring@Jamess-MacBook-Pro-2.local>
2021-08-11 23:45:05 +0100

    Front end nested folders.

--- website/static/website/folder_list.js
+++ website/static/website/folder_list.js
@@ -31,10 +31,19 @@ $(function() {
         var row = $(this).parents(".table-row")[0];
         var entry_id = $(row).data("id");
 
-        // Check if the group already exists
-        var group = $(this).parents(".table").find("div.table-row-group[data-parent='" + entry_id + "']");
+        // The folder depth of this row
+        var depth = Number($(row).data("depth") || 0);
+
+        // Check if the group of rows already exists
+        var group = $(row).nextUntil("[data-depth=" + String(depth) + "]");
         if (group.length > 0) {
-            $(group).toggleClass("hidden");
+            if ($(group[0]).hasClass("hidden")) {
+                // First entry is hidden so the folder must be collapsed. Need to expand to show the next level
+                $(group).filter("[data-depth=" + String(depth + 1) + "]").removeClass("hidden");
+            } else {
+                // First entry is visible so the folder must be open. Need to collapse it.
+                $(group).addClass("hidden");
+            }
             return;
         }
 
@@ -51,38 +60,47 @@ $(function() {
             function(response) {
                 // Append the rows
 
-                // Create a row group
-                var row_group = $("<div class='table-row-group' data-parent='" + entry_id + "'></div>");
-
                 var sub_table = $(response.html);
 
                 // Get the rows (ignoring header row)
                 var rows = $(sub_table).find(".table-row").slice(1);
 
                 // Remove cells with wrong name
+                var previousRow = row;
                 for (var i = 0; i < rows.length; i++) {
                     var new_row = rows[i];
+
+                    // Set the depth
+                    $(new_row).attr("data-depth", depth + 1);
+
                     var cells = $(new_row).find(".table-cell");
+
                     for (var j = 0; j < cells.length; j++) {
                         var cell = cells[j];
                         var name = $(cell).data("name");
+
                         if (!columns.includes(name)) {
                             // This cell is not included
                             $(cell).remove();
+                            continue;
+                        }
+
+                        if (name == "order") {
+                            // Don't order sub-files
+                            $(cell).remove();
+                            continue;
                         }
 
                         // Increase margin
                         if (name == "name") {
-                            $(cell).css("padding-left", "+=2rem");
+                            $(cell).css("padding-left", String((depth + 1) * 2) + "rem");
                         }
                     }
 
                     // Add the row
-                    $(row_group).append(new_row);
+                    $(new_row).insertAfter(previousRow);
+                    previousRow = new_row;
                 }
-
-                // Add the row group
-                $(row_group).insertAfter(row);
 
             }
         )
