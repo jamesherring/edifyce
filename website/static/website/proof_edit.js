4faac5f602b750d60451da1e117701a98318d6f3
James Herring <jamesherring888@gmail.com>
2021-10-23 19:53:54 +0200

    Collect static

--- website/static/website/proof_edit.js
+++ website/static/website/proof_edit.js
@@ -105,7 +105,7 @@ $(function() {
     editor.editor.session.on("change", function(e) {
         // Validate the proof 500ms after any changes
         window.clearTimeout(window.timeout);
-        window.timeout = setTimeout(validate_proof, 250);
+        window.timeout = setTimeout(validate_proof, 1000);
 
     });
 
